25dada42161b5c987275de0ff7cb98d8f9d1bfc7
Julian Garcia <jugar28@gmail.com>
2021-11-05 23:07:37 -0400

    added comment

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -1,6 +1,7 @@
 let mongoose = require('mongoose');
 require('dotenv').config()
 
+//Simplified database connection 
 mongoose.connect(`${process.env.DATABASE}`);
 
 let CustomerSchema = new mongoose.Schema({
